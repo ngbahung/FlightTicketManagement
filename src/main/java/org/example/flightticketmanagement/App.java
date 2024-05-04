9d66c165cd1fb716df91373f9904fbae83b5a018
Nguyen Ba Hung <22520512@gm.uit.edu.vn>
2024-05-04 22:30:08 +0700

    update

--- src/main/java/org/example/flightticketmanagement/App.java
+++ src/main/java/org/example/flightticketmanagement/App.java
@@ -1,5 +1,8 @@
 package org.example.flightticketmanagement;
 
+import io.github.palexdev.materialfx.theming.JavaFXThemes;
+import io.github.palexdev.materialfx.theming.MaterialFXStylesheets;
+import io.github.palexdev.materialfx.theming.UserAgentBuilder;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -11,6 +14,15 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
+
+        UserAgentBuilder.builder()
+                .themes(JavaFXThemes.MODENA)
+                .themes(MaterialFXStylesheets.forAssemble(true))
+                .setDeploy(true)
+                .setResolveAssets(true)
+                .build()
+                .setGlobal();
+        
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Fxml/DangNhap.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
