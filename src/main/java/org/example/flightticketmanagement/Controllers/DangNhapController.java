147cfc3db4047d86e93c44839fe304e77bd5b869
trinhtrinh342004 <trinhnguyen8cbm@gmail.com>
2024-05-04 18:49:47 +0700

    complete DangNhap.fxml

--- src/main/java/org/example/flightticketmanagement/Controllers/DangNhapController.java
+++ src/main/java/org/example/flightticketmanagement/Controllers/DangNhapController.java
@@ -1,4 +1,17 @@
 package org.example.flightticketmanagement.Controllers;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class DangNhapController {
+    public Label tenDangNhap_lbl;
+    public Label tenTaiKhoan_lbl;
+    public TextField taiKhoan_fld;
+    public Label matKhau_lbl;
+    public PasswordField matKhau_fld;
+    public Button dangNhap_btn;
+    public Label loi_lbl;
+
 }
