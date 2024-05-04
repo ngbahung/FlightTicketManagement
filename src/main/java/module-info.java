461fceecd9505dc03823fdca3dabc9d007c6fcfd
trinhtrinh342004 <trinhnguyen8cbm@gmail.com>
2024-05-04 23:09:27 +0700

    change module.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,5 +7,10 @@ module org.example.flightticketmanagement {
     opens org.example.flightticketmanagement to javafx.fxml;
     exports org.example.flightticketmanagement.Controllers to javafx.fxml;
     exports org.example.flightticketmanagement;
+    exports org.example.flightticketmanagement.Controllers.Admin;
+    exports org.example.flightticketmanagement.Controllers.Manager;
+    exports org.example.flightticketmanagement.Controllers.Staff;
+    exports org.example.flightticketmanagement.Models;
+    exports org.example.flightticketmanagement.Views;
 
 }
