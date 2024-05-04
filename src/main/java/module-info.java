b098cb3a0d2eccb5c0bf778154fded734a5b1c6b
Nguyen Ba Hung <22520512@gm.uit.edu.vn>
2024-05-04 15:08:05 +0700

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.example.flightticketmanagement {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens org.example.flightticketmanagement to javafx.fxml;
+    exports org.example.flightticketmanagement;
+}
