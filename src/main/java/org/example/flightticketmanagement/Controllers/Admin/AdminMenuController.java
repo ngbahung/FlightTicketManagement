9371293100b52ca8219a12bb342cce61f847bc82
trinhtrinh342004 <trinhnguyen8cbm@gmail.com>
2024-05-08 15:00:27 +0700

    change adminmenu

--- src/main/java/org/example/flightticketmanagement/Controllers/Admin/AdminMenuController.java
+++ src/main/java/org/example/flightticketmanagement/Controllers/Admin/AdminMenuController.java
@@ -1,4 +1,12 @@
 package org.example.flightticketmanagement.Controllers.Admin;
 
+import javafx.scene.control.Button;
+
 public class AdminMenuController {
+    public Button phanQuyen_btn;
+    public Button trangChu_btn;
+    public Button traCuu_btn;
+    public Button doanhThu_btn;
+    public Button suaQuyDinh_btn;
+    public Button dangXuat_btn;
 }
