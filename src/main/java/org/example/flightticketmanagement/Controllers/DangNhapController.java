ed2249255bd44b4c61b6045a3b4b92958e1519dd
trinhtrinh342004 <trinhnguyen8cbm@gmail.com>
2024-05-08 15:03:56 +0700

    craft

--- src/main/java/org/example/flightticketmanagement/Controllers/DangNhapController.java
+++ src/main/java/org/example/flightticketmanagement/Controllers/DangNhapController.java
@@ -1,11 +1,7 @@
 package org.example.flightticketmanagement.Controllers;
 
-<<<<<<< Updated upstream
 import io.github.palexdev.materialfx.controls.MFXPasswordField;
 import io.github.palexdev.materialfx.controls.MFXTextField;
-=======
-import io.github.palexdev.materialfx.controls.MFXButton;
->>>>>>> Stashed changes
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
@@ -15,16 +11,9 @@ public class DangNhapController {
     public Label tenDangNhap_lbl;
     public Label tenTaiKhoan_lbl;
     public Label matKhau_lbl;
-<<<<<<< Updated upstream
     public Button dangNhap_btn;
-=======
-    public PasswordField matKhau_fld;
->>>>>>> Stashed changes
     public Label loi_lbl;
-    public MFXButton dangNhap_btn;
-
 
     public MFXTextField tenTK_mfxfld;
     public MFXPasswordField matKhau_mfxpassfld;
-
 }
