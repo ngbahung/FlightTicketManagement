930ddec9dd23c4f5e0b9b91e4a91205dea93e0db
trinhtrinh342004 <trinhnguyen8cbm@gmail.com>
2024-05-09 00:58:33 +0700

    change TrangChu

--- src/main/java/org/example/flightticketmanagement/Controllers/Admin/TrangChuController.java
+++ src/main/java/org/example/flightticketmanagement/Controllers/Admin/TrangChuController.java
@@ -1,4 +1,9 @@
 package org.example.flightticketmanagement.Controllers.Admin;
 
+import javafx.scene.control.Button;
+
 public class TrangChuController {
+    public Button lichChuyenBay_btn;
+    public Button banVe_btn;
+    public Button datCho_btn;
 }
--- src/main/java/org/example/flightticketmanagement/Controllers/Admin/TrangChuLichController.java
+++ src/main/java/org/example/flightticketmanagement/Controllers/Admin/TrangChuLichController.java
@@ -1,4 +1,30 @@
 package org.example.flightticketmanagement.Controllers.Admin;
 
+import io.github.palexdev.materialfx.controls.MFXButton;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class TrangChuLichController {
+    public MFXButton themNhieu_btn;
+    public MFXButton xoaSua_btn;
+    public MFXButton taiFileExel_btn;
+    public MFXButton luu_btn;
+    public Label maChuyenBay_lbl;
+    public Label sanBayDi_lbl;
+    public Label NgayGio_lbl;
+    public Label soLuongHangGhe1_lbl;
+    public Label giaVe_lbl;
+    public Label sanBayDen_lbl;
+    public Label thoiGianBay_lbl;
+    public Label soLuongHangGhe2_lbl;
+    public TextField maCB_txtfld;
+    public TextField sanBayDi_txtfld;
+    public TextField ngayGio_txtfld;
+    public TextField soLuongHangGhe1_txtfld;
+    public TextField giaVe_txtfld;
+    public TextField sanBayDen_txtfld;
+    public TextField thoiGianBay_fld;
+    public TextField soLuongHangGhe2_txtfld;
+    public CheckBox sanBayTrungGian_chkbox;
 }
