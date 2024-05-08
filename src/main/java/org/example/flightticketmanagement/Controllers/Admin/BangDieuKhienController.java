81ee879e84e0bfdc5b5396e3cc6893688852fe5c
trinhtrinh342004 <trinhnguyen8cbm@gmail.com>
2024-05-08 15:22:18 +0700

    thay doi BangDieuKhienController

--- src/main/java/org/example/flightticketmanagement/Controllers/Admin/BangDieuKhienController.java
+++ src/main/java/org/example/flightticketmanagement/Controllers/Admin/BangDieuKhienController.java
@@ -1,4 +1,9 @@
 package org.example.flightticketmanagement.Controllers.Admin;
 
+import io.github.palexdev.materialfx.controls.MFXButton;
+
 public class BangDieuKhienController {
+    public MFXButton them_btn;
+    public MFXButton xoa_btn;
+    public MFXButton sua_btn;
 }
