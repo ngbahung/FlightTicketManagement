67a1f011142bff03cf6ac8d371ffbffa0203dd72
Nguyen Ba Hung <22520512@gm.uit.edu.vn>
2024-05-20 23:34:03 +0700

    Update QuyDinhController.java

--- src/main/java/org/example/flightticketmanagement/Controllers/Admin/QuyDinhController.java
+++ src/main/java/org/example/flightticketmanagement/Controllers/Admin/QuyDinhController.java
@@ -15,6 +15,7 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import org.example.flightticketmanagement.Controllers.AlertMessage;
 import org.example.flightticketmanagement.Models.DatabaseDriver;
+import org.example.flightticketmanagement.Models.HangVe;
 import org.example.flightticketmanagement.Models.SanBay;
 
 import java.io.IOException;
@@ -26,7 +27,7 @@ import java.util.ResourceBundle;
 public class QuyDinhController implements Initializable {
 
     @FXML
-    private TableView<?> hangve_tbv;
+    private TableView<HangVe> hangve_tbv;
 
     @FXML
     private TableColumn<?, ?> heso_col;
@@ -62,6 +63,9 @@ public class QuyDinhController implements Initializable {
     private TableView<SanBay> sanbay_tbv;
 
     @FXML
+    private Button searchHangVe_btn;
+
+    @FXML
     private Button searchSanBay_btn;
 
     @FXML
@@ -71,7 +75,10 @@ public class QuyDinhController implements Initializable {
     private TextField searchsanbay_tfx;
 
     @FXML
-    private TableColumn<?, ?> stthgve_col;
+    private TableColumn<HangVe, Integer> statusHangVe_col;
+
+    @FXML
+    private TableColumn<SanBay, Integer> status_col;
 
     @FXML
     private TableColumn<?, ?> sttsanbay_col;
@@ -101,6 +108,9 @@ public class QuyDinhController implements Initializable {
     private TableColumn<?, ?> tinhthanh_col;
 
     @FXML
+    private TableColumn<?, ?> vietTat_col;
+
+    @FXML
     private Button xoahangve_btn;
 
     @FXML
@@ -114,27 +124,86 @@ public class QuyDinhController implements Initializable {
 
     private final AlertMessage alert = new AlertMessage();
 
-    private FilteredList<SanBay> filteredData;
+    private FilteredList<SanBay> filteredSanBayData;
+    private FilteredList<HangVe> filteredHangVeData;
 
-    private ObservableList<SanBay> hangVeList = FXCollections.observableArrayList();
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         showSanBayList();
         showHangVeList();
         suasanbay_btn.setOnAction(this::editSelectedAirports);
-        xoasanbay_btn.setOnAction(this::deleteSelectedAirports);
-        refreshSanBayData_btn.setOnAction(event -> showSanBayList());
+        xoasanbay_btn.setOnAction(this::deactivateSelectedAirports);
         searchSanBay_btn.setOnAction(this::searchSanBay);
+        searchHangVe_btn.setOnAction(this::searchHangVe);
+        refreshSanBayData_btn.setOnAction(e -> showSanBayList());
+        refreshHangVeData_btn.setOnAction(e -> showHangVeList());
     }
 
     private void showHangVeList() {
+        ObservableList<HangVe> hangVeList = FXCollections.observableArrayList();
+
+        String query = "SELECT * FROM HANGVE";
+
+        try {
+            connect = DatabaseDriver.getConnection();
+            statement = connect.createStatement();
+            result = statement.executeQuery(query);
+
+            while (result.next()) {
+                String maHangVe = result.getString("MaHangVe");
+                String tenHangVe = result.getString("TenHangVe");
+                float heSo = result.getFloat("HeSo");
+                int trangThai = result.getInt("TrangThai");
+
+                HangVe hangVe = new HangVe(maHangVe, tenHangVe, heSo, trangThai);
+                hangVeList.add(hangVe);
+            }
+
+            tenhangve_col.setCellValueFactory(new PropertyValueFactory<>("tenHangVe"));
+            heso_col.setCellValueFactory(new PropertyValueFactory<>("heSo"));
+            statusHangVe_col.setCellValueFactory(new PropertyValueFactory<>("trangThai"));
+
+            statusHangVe_col.setCellFactory(column -> new TableCell<HangVe, Integer>() {
+                @Override
+                protected void updateItem(Integer item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if (item == null || empty) {
+                        setText(null);
+                    } else {
+                        if (item == 0) {
+                            setText("Ngưng hoạt động");
+                            setTextFill(javafx.scene.paint.Color.RED);
+                        } else {
+                            setText("Hoạt động");
+                            setTextFill(javafx.scene.paint.Color.GREEN);
+                        }
+                    }
+                }
+            });
+
+            hangve_tbv.setItems(hangVeList);
+            filteredHangVeData = new FilteredList<>(hangVeList, p -> true);
+            hangve_tbv.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (result != null) result.close();
+                if (statement != null) statement.close();
+                if (connect != null) connect.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     private void showSanBayList() {
         ObservableList<SanBay> sanBayList = FXCollections.observableArrayList();
 
-        String query = "SELECT * FROM SANBAY WHERE TrangThai = 1 ORDER BY MaSanBay ASC";
+        String query = "SELECT * FROM SANBAY";
 
         try {
             connect = DatabaseDriver.getConnection();
@@ -159,10 +228,29 @@ public class QuyDinhController implements Initializable {
             tensanbay_col.setCellValueFactory(new PropertyValueFactory<>("tenSanBay"));
             tinhthanh_col.setCellValueFactory(new PropertyValueFactory<>("diaChi"));
             sttsanbay_col.setCellValueFactory(new PropertyValueFactory<>("soThuTu"));
+            vietTat_col.setCellValueFactory(new PropertyValueFactory<>("tenVietTat"));
 
+            status_col.setCellValueFactory(new PropertyValueFactory<>("trangThai"));
+            status_col.setCellFactory(column -> new TableCell<SanBay, Integer>() {
+                @Override
+                protected void updateItem(Integer item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if (item == null || empty) {
+                        setText(null);
+                    } else {
+                        if (item == 0) {
+                            setText("Ngưng hoạt động");
+                            setTextFill(javafx.scene.paint.Color.RED); // Set text color to red
+                        } else {
+                            setText("Hoạt động");
+                            setTextFill(javafx.scene.paint.Color.GREEN); // Set text color to green
+                        }
+                    }
+                }
+            });
 
             sanbay_tbv.setItems(sanBayList);
-            filteredData = new FilteredList<>(sanBayList, p -> true);
+            filteredSanBayData = new FilteredList<>(sanBayList, p -> true);
             sanbay_tbv.getSelectionModel().setSelectionMode(javafx.scene.control.SelectionMode.MULTIPLE);  // Allow multiple selection
 
         } catch (SQLException e) {
@@ -176,8 +264,6 @@ public class QuyDinhController implements Initializable {
                 e.printStackTrace();
             }
         }
-
-
     }
 
     @FXML
@@ -200,42 +286,61 @@ public class QuyDinhController implements Initializable {
     }
 
     @FXML
-    void deleteSelectedAirports(ActionEvent event) {
+    void addHangVe(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Admin/ThemHangVe.fxml"));
+            Parent root = loader.load();
+            ThemHangVeController themHangVeController = loader.getController();
+            themHangVeController.setParentController(this);  // Pass the instance of QuyDinhController
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.setTitle("Thêm Hạng Vé");
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+        @FXML
+        void deactivateSelectedAirports(ActionEvent event) {
         ObservableList<SanBay> selectedAirports = sanbay_tbv.getSelectionModel().getSelectedItems();
 
         if (selectedAirports.isEmpty()) {
-            alert.errorMessage("Không chọn sân bay nào. Vui lòng chọn ít nhất một sân bay để xóa.");
+            alert.errorMessage("Không chọn sân bay nào. Vui lòng chọn ít nhất một sân bay.");
             return;
         }
 
         Alert confirmationAlert = new Alert(Alert.AlertType.CONFIRMATION);
-        confirmationAlert.setTitle("Xác nhận xóa");
-        confirmationAlert.setHeaderText("Bạn có muốn xóa những sân bay đã chọn?");
-        confirmationAlert.setContentText("Hành động này không thể hoàn tác.");
+        confirmationAlert.setTitle("Xác nhận cập nhật trạng thái");
+        confirmationAlert.setHeaderText("Bạn có muốn thay đổi trạng thái của các sân bay đã chọn?");
+        confirmationAlert.setContentText("Hành động này sẽ thay đổi trạng thái của sân bay từ 'Ngưng hoạt động' sang 'Đang hoạt động' và ngược lại.");
 
         Optional<ButtonType> result = confirmationAlert.showAndWait();
         if (result.isPresent() && result.get() == ButtonType.OK) {
             try {
                 connect = DatabaseDriver.getConnection();
-                prepare = connect.prepareStatement("UPDATE SANBAY SET TrangThai = 0 WHERE MaSanBay = ?");
+                prepare = connect.prepareStatement("UPDATE SANBAY SET TrangThai = ? WHERE MaSanBay = ?");
 
                 for (SanBay sanBay : selectedAirports) {
-                    prepare.setString(1, sanBay.getMaSanBay());
+                    int newStatus = sanBay.getTrangThai() == 0 ? 1 : 0;
+                    prepare.setInt(1, newStatus);
+                    prepare.setString(2, sanBay.getMaSanBay());
                     prepare.addBatch();
                 }
 
                 int[] results = prepare.executeBatch();
                 if (results.length > 0) {
-                    alert.successMessage("Sân bay đã được xóa thành công.");
+                    alert.successMessage("Trạng thái sân bay đã được cập nhật thành công.");
                 } else {
-                    alert.errorMessage("Không xóa được sân bay đã chọn.");
+                    alert.errorMessage("Không cập nhật được trạng thái sân bay đã chọn.");
                 }
 
                 showSanBayList();  // Refresh the table
 
             } catch (SQLException e) {
                 e.printStackTrace();
-                alert.errorMessage("Lỗi khi đang xóa sân bay. Vui lòng kiểm tra lại.");
+                alert.errorMessage("Lỗi khi đang cập nhật trạng thái sân bay. Vui lòng kiểm tra lại.");
             } finally {
                 try {
                     if (prepare != null) prepare.close();
@@ -276,15 +381,20 @@ public class QuyDinhController implements Initializable {
         showSanBayList();
     }
 
+    @FXML
+    void refreshHangVeData() {
+        showHangVeList();
+    }
+
     private void searchSanBay(ActionEvent e) {
         String keyword = searchsanbay_tfx.getText();
         if (keyword == null || keyword.isEmpty()) {
-            sanbay_tbv.setItems(filteredData);
+            sanbay_tbv.setItems(filteredSanBayData);
             return;
         }
 
         String lowerCaseFilter = keyword.toLowerCase();
-        filteredData.setPredicate(sanBay -> {
+        filteredSanBayData.setPredicate(sanBay -> {
             if (sanBay.getMaSanBay().toLowerCase().contains(lowerCaseFilter) ||
                     sanBay.getTenSanBay().toLowerCase().contains(lowerCaseFilter) ||
                     sanBay.getDiaChi().toLowerCase().contains(lowerCaseFilter)) {
@@ -293,8 +403,105 @@ public class QuyDinhController implements Initializable {
             return false;
         });
 
-        SortedList<SanBay> sortedData = new SortedList<>(filteredData);
+        SortedList<SanBay> sortedData = new SortedList<>(filteredSanBayData);
         sortedData.comparatorProperty().bind(sanbay_tbv.comparatorProperty());
         sanbay_tbv.setItems(sortedData);
     }
+
+    private void searchHangVe(ActionEvent e){
+        String keyword = searchhangve_tfx.getText();
+        if (keyword == null || keyword.isEmpty()) {
+            hangve_tbv.setItems(filteredHangVeData);
+            return;
+        }
+
+        String lowerCaseFilter = keyword.toLowerCase();
+        filteredHangVeData.setPredicate(hangVe -> {
+            if (hangVe.getMaHangVe().toLowerCase().contains(lowerCaseFilter) ||
+                    hangVe.getTenHangVe().toLowerCase().contains(lowerCaseFilter)) {
+                return true;
+            }
+            return false;
+        });
+
+        SortedList<HangVe> sortedData = new SortedList<>(filteredHangVeData);
+        sortedData.comparatorProperty().bind(hangve_tbv.comparatorProperty());
+        hangve_tbv.setItems(sortedData);
+    }
+
+    @FXML
+    void deactiveSelectedTicketClass(ActionEvent event) {
+        ObservableList<HangVe> selectedTicketClasses = hangve_tbv.getSelectionModel().getSelectedItems();
+
+        if (selectedTicketClasses.isEmpty()) {
+            alert.errorMessage("Không chọn hạng vé nào. Vui lòng chọn ít nhất một hạng vé.");
+            return;
+        }
+
+        Alert confirmationAlert = new Alert(Alert.AlertType.CONFIRMATION);
+        confirmationAlert.setTitle("Xác nhận cập nhật trạng thái");
+        confirmationAlert.setHeaderText("Bạn có muốn thay đổi trạng thái của các hạng vé đã chọn?");
+        confirmationAlert.setContentText("Hành động này sẽ thay đổi trạng thái của hạng vé từ 'Ngưng hoạt động' sang 'Đang hoạt động' và ngược lại.");
+
+        Optional<ButtonType> result = confirmationAlert.showAndWait();
+        if (result.isPresent() && result.get() == ButtonType.OK) {
+            try {
+                connect = DatabaseDriver.getConnection();
+                prepare = connect.prepareStatement("UPDATE HANGVE SET TrangThai = ? WHERE MaHangVe = ?");
+
+                for (HangVe hangVe : selectedTicketClasses) {
+                    int newStatus = hangVe.getTrangThai() == 0 ? 1 : 0;
+                    prepare.setInt(1, newStatus);
+                    prepare.setString(2, hangVe.getMaHangVe());
+                    prepare.addBatch();
+                }
+
+                int[] results = prepare.executeBatch();
+                if (results.length > 0) {
+                    alert.successMessage("Trạng thái hạng vé đã được cập nhật thành công.");
+                } else {
+                    alert.errorMessage("Không cập nhật được trạng thái hạng vé đã chọn.");
+                }
+
+                showHangVeList();  // Refresh the table
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+                alert.errorMessage("Lỗi khi đang cập nhật trạng thái hạng vé. Vui lòng kiểm tra lại.");
+            } finally {
+                try {
+                    if (prepare != null) prepare.close();
+                    if (connect != null) connect.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @FXML
+    void editSelectedTicketClass(ActionEvent event) {
+        HangVe selectedTicketClass = hangve_tbv.getSelectionModel().getSelectedItem();
+        if (selectedTicketClass == null) {
+            alert.errorMessage("Không chọn hạng vé nào. Vui lòng chọn một hạng vé để sửa.");
+            return;
+        }
+
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Admin/SuaHangVe.fxml"));
+            Parent root = loader.load();
+            SuaHangVeController suaHangVeController = loader.getController();
+            suaHangVeController.setHangVe(selectedTicketClass);  // Pass the selected ticket class to SuaHangVeController
+            suaHangVeController.setParentController(this);  // Pass the instance of QuyDinhController
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.setTitle("Sửa Hạng Vé");
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
