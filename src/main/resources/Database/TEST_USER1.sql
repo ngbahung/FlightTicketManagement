2760fed124cd17d3bdaad7d606c017c71db81b10
Nguyen Ba Hung <22520512@gm.uit.edu.vn>
2024-06-13 10:07:43 +0700

    Update TEST_USER1.sql

--- src/main/resources/Database/TEST_USER1.sql
+++ src/main/resources/Database/TEST_USER1.sql
@@ -1,4 +1,4 @@
-ALTER SESSION SET CURRENT_SCHEMA = fly_the_end12A;
+ALTER SESSION SET CURRENT_SCHEMA = fly_the_end12B;
 
 set transaction isolation level read committed;
 
@@ -17,7 +17,7 @@ commit;
 SELECT *
 FROM BAOCAOTHANG 
 WHERE Thang = 6  
-AND Nam = 2024
+AND Nam = 2024;
 
 
 SELECT * FROM VE WHERE MACHUYENBAY = 'CB052';
@@ -54,16 +54,15 @@ END;
 -------------------------------------------------------------------
 -- Lost update Trung
 -- Thiết lập mức độ cách ly giao dịch SERIALIZABLE
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
 SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;
-set AUTOCOMMIT off;
-set AUTOCOMMIT on;
+SET AUTOCOMMIT OFF;
+SET AUTOCOMMIT ON;
 
 
 SELECT * FROM CT_DATVE WHERE MACT_DATVE = 'CTDV05302';
 select * from baocaothang 
 WHERE machuyenbay = 'CB047' AND thang = 6 AND nam = 2024;
-
-DELETE FROM CT_DATVE WHERE MaCT_DATVE = 'CTDV04702';
 
 
 EXECUTE update_ticket_status('CTDV05302', 2);
@@ -86,7 +85,7 @@ from ct_datve
 where mact_datve ='CTDV05002';
 
 select * from baocaothang WHERE thang = 6 AND nam = 2024
-AND machuyenbay = 'CB050';
+AND machuyenbay = 'CB052';
 
 Exec UPDATE_TICKET_STATUS('CTDV05002',2);
 
@@ -101,3 +100,74 @@ where mahangve
 ='HV002';
 
 commit;
+
+
+-------------------------------------------------------------------
+-- TEST LOST UPDATE
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;
+SET AUTOCOMMIT OFF;
+SET AUTOCOMMIT ON;
+COMMIT;
+
+SELECT * FROM CT_DATVE 
+WHERE MACT_DATVE = 'CTDV05210';
+
+
+SELECT * FROM BAOCAOTHANG
+WHERE THANG = 6 AND NAM = 2024
+AND MACHUYENBAY = 'CB052';
+
+EXECUTE UPDATE_TICKET_STATUS('CTDV05210', 2);
+EXECUTE UPDATE_TICKET_STATUS('CTDV05211', 2);
+
+
+-------------------------------------------------------------------
+-- TEST NON-REPEATABLE READ
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;
+SET AUTOCOMMIT OFF;
+SET AUTOCOMMIT ON;
+COMMIT;
+
+SELECT *
+FROM BAOCAOTHANG 
+WHERE Thang = 6  
+AND Nam = 2024;
+
+SELECT * FROM CT_DATVE 
+     JOIN VE
+     ON VE.mAvE = CT_DATVE.MAVE
+WHERE EXTRACT(YEAR FROM NGAYTHANHTOAN) = 2024 AND EXTRACT(MONTH FROM NGAYTHANHTOAN) = 6;
+
+-------------------------------------------------------------------
+-- TEST PHANTOM READ
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;
+SET AUTOCOMMIT OFF;
+SET AUTOCOMMIT ON;
+COMMIT;
+
+SELECT *
+FROM BAOCAOTHANG 
+WHERE Thang = 6  
+AND Nam = 2024;
+
+SELECT * FROM CHUYENBAY WHERE MACHUYENBAY = 'CB022';
+-------------------------------------------------------------------
+-- TEST DEADLOCK
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;
+SET AUTOCOMMIT OFF;
+SET AUTOCOMMIT ON;
+COMMIT;
+
+update hangve
+set trangthai = 0
+where mahangve
+='HV001';
+
+update hangve
+set trangthai = 0
+where mahangve
+='HV002';
