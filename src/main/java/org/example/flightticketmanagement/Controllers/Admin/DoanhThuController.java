fdf0655a541e5926462f96469c7c463cd66a54d9
Nguyen Ba Hung <22520512@gm.uit.edu.vn>
2024-06-13 10:16:14 +0700

    Update DoanhThuController.java

--- src/main/java/org/example/flightticketmanagement/Controllers/Admin/DoanhThuController.java
+++ src/main/java/org/example/flightticketmanagement/Controllers/Admin/DoanhThuController.java
@@ -128,7 +128,7 @@ public class DoanhThuController implements Initializable {
                 "ORDER BY" +
                 "    m.Thang";
         try {
-            connect.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+//            connect.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
             prepare = connect.prepareStatement(query);
             prepare.setInt(1, namBaoCao);
             try (ResultSet result = prepare.executeQuery()) {
@@ -202,7 +202,7 @@ public class DoanhThuController implements Initializable {
 
 
         try {
-            connect.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+//            connect.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
             prepare = connect.prepareStatement(query);
             prepare.setInt(1, DTN_namBaoCao);
             try (ResultSet result = prepare.executeQuery()) {
@@ -277,7 +277,7 @@ public class DoanhThuController implements Initializable {
                 "WHERE Thang = ? AND Nam = ?";
 
         try {
-            connect.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+//            connect.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
             prepare = connect.prepareStatement(query);
             prepare.setInt(1, thangBaoCao);
             prepare.setInt(2, namBaoCao);
@@ -344,7 +344,7 @@ public class DoanhThuController implements Initializable {
                 "WHERE Thang = ? AND Nam = ?";
 
         try  {
-            connect.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+//            connect.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
             prepare = connect.prepareStatement(query);
             prepare.setInt(1, DTT_thangBaoCao);
             prepare.setInt(2, DTT_namBaoCao);
